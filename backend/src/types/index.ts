3b10305a3d11f0e2da287275a7fdf5b29b18d4c0
Sanka-Vamsi-Akshay <sankavamsiakshay@gmail.com>
2026-06-28 21:37:50 +0530

    Fix Render production build dependencies

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,10 +1,21 @@
 import { Request } from 'express';
+import 'multer';
 
 export interface UserPayload {
   id: string;
   role: string;
 }
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
+
 export interface AuthenticatedRequest extends Request {
   user?: UserPayload;
+  file?: Express.Multer.File;
+  files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
 }
